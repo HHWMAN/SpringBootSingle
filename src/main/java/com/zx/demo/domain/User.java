4a39da5c7ca3fd3050cdf67db0967d27aa01388d
ZHANGXIANG <863413076@qq.com>
2019-03-05 08:59:26 +0800

    lombok生成set/get

--- src/main/java/com/zx/demo/domain/User.java
+++ src/main/java/com/zx/demo/domain/User.java
@@ -1,8 +1,11 @@
 package com.zx.demo.domain;
 
 
+import lombok.Data;
+
 import javax.persistence.*;
 
+@Data
 @Entity
 @Table(name="t_user")
 public class User {
@@ -19,39 +22,6 @@ public class User {
 
     @Column(name = "t_address")
     private String address;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
 
 }
 
